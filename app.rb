7bfc20247c7c1dc1fa11aea91f0c5f0db329df30
mtc-codes <82724982+mtc-codes@users.noreply.github.com>
2024-10-24 00:33:02 +0000

    Almost there, just need to figure out how to make a dynamic list again

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+
+get("/process_roll") do
+  @num_dice = params.fetch("number").to_i
+  @sides_dice = params.fetch("count").to_i
+  @rolls = []
+  @num_dice.times do
+      die = rand(1..@sides_dice)
+  end
+  
+  erb(:process_roll)
 end
